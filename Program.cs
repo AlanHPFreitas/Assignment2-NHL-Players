fb5c1ce49cb391813878a1023922a7e9ba190bf8
AlanHPFreitas <131428856+AlanHPFreitas@users.noreply.github.com>
2024-10-28 15:08:02 -0600

    test direct push to master

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Assignment2_NHL_Players
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
+        }aaaaa
     }
 }
 
