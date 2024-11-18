b3f38220229762f76ee5aa063d6a3aee19d3061d
AlanHPFreitas <131428856+AlanHPFreitas@users.noreply.github.com>
2024-11-18 16:09:52 -0700

    Design changes

--- Form/Form1.Designer.cs
+++ Form/Form1.Designer.cs
@@ -32,24 +32,26 @@
             this.TxtFilter = new System.Windows.Forms.TextBox();
             this.dgvPlayerStats = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPlayerStats)).BeginInit();
             this.SuspendLayout();
             // 
             // TxtSort
             // 
-            this.TxtSort.Location = new System.Drawing.Point(612, 18);
-            this.TxtSort.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtSort.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSort.Location = new System.Drawing.Point(865, 20);
             this.TxtSort.Name = "TxtSort";
-            this.TxtSort.Size = new System.Drawing.Size(218, 29);
+            this.TxtSort.Size = new System.Drawing.Size(306, 45);
             this.TxtSort.TabIndex = 1;
-            this.TxtSort.Text = "G des";
+            this.TxtSort.Text = "G desc";
             // 
             // TxtFilter
             // 
-            this.TxtFilter.Location = new System.Drawing.Point(874, 18);
-            this.TxtFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtFilter.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtFilter.Location = new System.Drawing.Point(375, 21);
             this.TxtFilter.Name = "TxtFilter";
-            this.TxtFilter.Size = new System.Drawing.Size(208, 29);
+            this.TxtFilter.Size = new System.Drawing.Size(277, 45);
             this.TxtFilter.TabIndex = 2;
             this.TxtFilter.Text = "G >= 50";
             // 
@@ -59,36 +61,64 @@
             this.dgvPlayerStats.AllowUserToOrderColumns = true;
             this.dgvPlayerStats.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgvPlayerStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPlayerStats.Location = new System.Drawing.Point(16, 76);
-            this.dgvPlayerStats.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvPlayerStats.Location = new System.Drawing.Point(26, 93);
             this.dgvPlayerStats.Name = "dgvPlayerStats";
             this.dgvPlayerStats.RowHeadersWidth = 51;
             this.dgvPlayerStats.RowTemplate.Height = 24;
-            this.dgvPlayerStats.Size = new System.Drawing.Size(1067, 580);
+            this.dgvPlayerStats.Size = new System.Drawing.Size(2000, 850);
             this.dgvPlayerStats.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(296, 22);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1215, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.Size = new System.Drawing.Size(223, 77);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Filter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Ravie", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(707, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 44);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Sort:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Ravie", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(202, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 44);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Filter:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1654, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TxtFilter);
             this.Controls.Add(this.TxtSort);
             this.Controls.Add(this.dgvPlayerStats);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPlayerStats)).EndInit();
             this.ResumeLayout(false);
@@ -101,6 +131,8 @@
         private System.Windows.Forms.TextBox TxtFilter;
         private System.Windows.Forms.DataGridView dgvPlayerStats;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
